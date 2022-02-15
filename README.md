43f2b93e7fc73bf2e3e3ea8d74665ad56c70a3ba
神代綺凛 <i@loli.best>
2022-02-15 18:10:41 +0800

    feat: mac support

--- README.md
+++ README.md
@@ -14,20 +14,22 @@
 
 > 目前只支持官服
 
-1. 下载工具后解压 - [下载地址](https://github.com/arkntools/arknights-gacha-export/releases/latest/download/Arknights-Gacha-Export.zip)
+1. 下载工具后解压 - [下载地址](https://github.com/arkntools/arknights-gacha-export/releases/latest)
 2. 点击工具的“加载数据”按钮，会弹出对话框要求输入 token
 3. 前往明日方舟官网 https://ak.hypergryph.com 登录，之后打开网址 https://as.hypergryph.com/user/info/v1/token_by_cookie 获取 token 并填入
 4. 如果没出什么问题的话，你会看到正在读取数据的提示，最终效果如下图所示
    <details>
     <summary>展开图片</summary>
 
-   再给 yj 充钱我就是狗.jpg  
    ![预览](/docs/preview.png)
    </details>
 
 如果需要导出多个账号的数据，可以点击旁边的加号按钮，再点击“加载数据”即可
 
-注意：官网只会保留 30 天内的 100 条抽卡记录（10 连只算 1 条），后续请自行注意数据更新周期以防遗漏
+### 注意事项
+
+1. 官网只会保留 30 天内的 100 条抽卡记录（10 连只算 1 条），后续请自行注意数据更新周期以防遗漏
+2. 由于官方寻访数据并未提供某卡池是否为标准寻访的信息，为方便计算，“累计 xx 抽未出”仅表示在同名卡池内的情况，不考虑跨池情况
 
 ## 定时更新寻访数据
 
@@ -37,11 +39,18 @@ Windows 可以利用**计划任务**来定时运行
 
 推荐频率每周一次
 
+## 用户数据
+
+包含设置、寻访数据、日志
+
+- Windows 位于 `%APPDATA%\app.lolicon.arknights-gacha-export`
+- MacOS 位于 `~/Library/Application Support/app.lolicon.arknights-gacha-export`
+
 ## TODO
 
 - [x] 按需拉取寻访数据
 - [x] 定时更新寻访数据
-- [ ] MacOS 支持
+- [x] MacOS 支持
 
 ## Development
 
--- src/main/update/index.js
+++ src/main/update/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 const fs = require('fs-extra')
 const extract = require('../module/extract-zip')
 const { version } = require('../../../package.json')
-const { hash, sendMsg } = require('../utils')
+const { hash, sendMsg, tempPath, pushLog, saveLog } = require('../utils')
 const config = require('../config')
 const i18n = require('../i18n')
 const streamPipeline = util.promisify(require('stream').pipeline)
@@ -22,10 +22,8 @@ const updateInfo = {
 }
 
 const isDev = !app.isPackaged
-const appPath = isDev ? path.resolve(__dirname, '../../', 'update-dev/app') : app.getAppPath()
-const updatePath = isDev
-  ? path.resolve(__dirname, '../../', 'update-dev/download')
-  : path.resolve(appPath, '..', '..', 'update')
+const appPath = isDev ? path.resolve(__dirname, '../../update-dev/app') : app.getAppPath()
+const updatePath = path.resolve(tempPath, 'update')
 
 const update = async () => {
   if (isDev) return
@@ -34,30 +32,40 @@ const update = async () => {
     const res = await fetch(`${url}/manifest.json?t=${Math.floor(Date.now() / (1000 * 60 * 10))}`)
     const data = await res.json()
     if (!data.active) return
-    if (semver.gt(data.version, version) && semver.gte(version, data.from)) {
-      await fs.emptyDir(updatePath)
-      const filePath = path.join(updatePath, data.name)
-      if (!config.autoUpdate) {
-        sendMsg(data.version, 'NEW_VERSION')
-        return
-      }
-      updateInfo.status = 'downloading'
-      await download(`${url}/${data.name}`, filePath)
-      const buffer = await fs.readFile(filePath)
-      const sha256 = hash(buffer)
-      if (sha256 !== data.hash) return
-      const appPathTemp = path.join(updatePath, 'app')
-      await extract(filePath, { dir: appPathTemp })
-      updateInfo.status = 'moving'
-      await fs.emptyDir(appPath)
-      await fs.copy(appPathTemp, appPath)
-      updateInfo.status = 'finished'
-      sendMsg(i18n.log.autoUpdate.success, 'UPDATE_HINT')
+    if (!(semver.gt(data.version, version) && semver.gte(version, data.from))) return
+    pushLog('INFO', 'New version found', data.version)
+    await fs.ensureDir(updatePath)
+    await fs.emptyDir(updatePath)
+    const filePath = path.join(updatePath, data.name)
+    if (!config.autoUpdate) {
+      sendMsg(data.version, 'NEW_VERSION')
+      return
     }
+    updateInfo.status = 'downloading'
+    const newPackageURL = `${url}/${data.name}`
+    pushLog('INFO', 'Downloading new version', newPackageURL)
+    await download(newPackageURL, filePath)
+    const buffer = await fs.readFile(filePath)
+    const sha256 = hash(buffer)
+    if (sha256 !== data.hash) {
+      pushLog('WARN', 'Hash check failed', sha256, data.hash)
+      return
+    }
+    const appPathTemp = path.join(updatePath, 'app')
+    pushLog('INFO', 'Extracting', filePath)
+    await extract(filePath, { dir: appPathTemp })
+    updateInfo.status = 'moving'
+    await fs.emptyDir(appPath)
+    pushLog('INFO', 'Copying to', appPath)
+    await fs.copy(appPathTemp, appPath)
+    updateInfo.status = 'finished'
+    pushLog('INFO', 'Update success')
+    sendMsg(i18n.log.autoUpdate.success, 'UPDATE_HINT')
   } catch (e) {
     updateInfo.status = 'failed'
     sendMsg(e, 'ERROR')
   }
+  saveLog()
 }
 
 const getUpdateInfo = () => updateInfo
